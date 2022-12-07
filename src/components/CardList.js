783808f715d37873c99849990dc05653f2d0afc4
Pablo Hidalgo <pablohidalgo.1983@gmail.com>
2022-12-07 09:33:49 -0600

    First Commit

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+    return ( 
+        <div>
+            {robots.map((user, i) =>  
+                <Card 
+                    key={i}
+                    id={robots[i].id}
+                    name={robots[i].name}
+                    username={robots[i].username}
+                    email={robots[i].email} 
+                />
+            )}
+        </div>
+     );
+}
+ 
+export default CardList;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Pepe",
+        username: "Pepe_El_Robot",
+        email: "pepeelrobot83@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Luis",
+        username: "Luis_El_Robot",
+        email: "luiselrobot83@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Pablo",
+        username: "Pablo_El_Robot",
+        email: "pabloelrobot83@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Lalo",
+        username: "Lalo_El_Robot",
+        email: "laloelrobot83@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Popo",
+        username: "Popo_El_Robot",
+        email: "popoelrobot83@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Chano",
+        username: "Chano_El_Robot",
+        email: "chanoelrobot83@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Kaka",
+        username: "Kaka_El_Robot",
+        email: "kakaelrobot83@gmail.com"
+    },
+    {
+        id: 8,
+        name: "Neymar",
+        username: "Neymar_El_Robot",
+        email: "neymarelrobot83@gmail.com"
+    },
+    {
+        id: 9,
+        name: "Haaland",
+        username: "Haaland_El_Robot",
+        email: "haalandelrobot83@gmail.com"
+    },
+    {
+        id: 10,
+        name: "Pony",
+        username: "Pony_El_Robot",
+        email: "ponyelrobot83@gmail.com"
+    }
+];
